3d35086483cbce60a4673347bca55a8e8df1240f
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-02-13 19:34:40 +0600

    add folder for the projects

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -3,7 +3,13 @@ interface Props {
 }
 
 const DashboardLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  <div className="flex h-screen overflow-hidden">
+    {/* Sidebar */}
+    <div className="flex flex-col flex-1 overflow-hidden">
+      {/* Header */}
+      <main className="flex-1 overflow-y-auto p-6">{children}</main>
+    </div>
+  </div>;
 };
 
 export default DashboardLayout;
